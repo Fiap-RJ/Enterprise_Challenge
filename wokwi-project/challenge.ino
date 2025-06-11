a7655e8479dd5324b66c51285c6a01e471a762c3
Michael Rodrigues <run.michael97@gmail.com>
2025-06-11 00:04:08 -0300

    feat: add new build and handle with challenge.ino

--- wokwi-project/challenge.ino
+++ wokwi-project/challenge.ino
@@ -14,20 +14,20 @@ const char *password = "SUA_SENHA";
 
 void setup()
 {
-    Serial.begin(115200);
-    dht.begin();
+  Serial.begin(115200);
+  dht.begin();
 
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.println("Conectando ao Wi-Fi...");
-    }
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Conectando ao Wi-Fi...");
+  }
 
-    Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());
 
-    server.on("/dados", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
+  server.on("/dados", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     float t = dht.readTemperature();
     float h = dht.readHumidity();
     if (isnan(t) || isnan(h)) {
@@ -37,5 +37,10 @@ void setup()
     String json = "{\"temperatura\":" + String(t) + ",\"umidade\":" + String(h) + "}";
     request->send(200, "application/json", json); });
 
-    server.begin();
+  server.begin();
+}
+
+void loop()
+{
+  // Obrigatório, mesmo que vazio
 }
